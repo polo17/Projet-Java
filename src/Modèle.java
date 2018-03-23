c6d3762ce0b809994fd98a10473b6d4c88818e36
Polina Polozyuk <ppolozyu@debian.univ-jfc.fr>
2018-03-23 15:29:46 +0100

    pb modele résolu

--- src/Modèle.java
+++ src/Modèle.java
@@ -1,13 +1,32 @@
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Observable;
 
 public class Modèle extends Observable {
 
+	public Hashtable<String, String> Nom_Images;
+	
+	public Hashtable<String, SimpleDateFormat> Dates;
+	
+	public Hashtable<String, Integer> Tailles;
+	
+	public Hashtable Couleurs;
+	
+	public Hashtable Lieux;
+		
+	
 	int sélectionnée;
 	ArrayList<Photo> images;
 
 	public Modèle() {
+		this.Nom_Images = new Hashtable<String, String>();
+		this.Dates = new Hashtable<String, SimpleDateFormat>();
+		this.Tailles = new Hashtable<String, Integer>();
+		//this.Couleurs = new Hashtable<>;
+		this.Lieux = new Hashtable<String, String>();
+		
 		File repertoire=new File("Images");
 		String[] noms=repertoire.list();
 		this.images=new ArrayList<>();
