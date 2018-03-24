e25c4bb655f4634736d5402525f0c1e61d72bdc2
AlexisHoch <alexis.hochart@gmail.com>
2018-03-24 03:44:21 +0100

    Ajout de la method de tri par ordre Alphabetique. Methode non testee

--- src/Modele.java
+++ src/Modele.java
@@ -4,34 +4,56 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Observable;
 
-public class Modèle extends Observable {
+@SuppressWarnings("deprecation")
+public class Modele extends Observable {
 
 	public Hashtable<String, String> Nom_Images;
-	
+
 	public Hashtable<String, SimpleDateFormat> Dates;
-	
+
 	public Hashtable<String, Integer> Tailles;
-	
+
 	public Hashtable Couleurs;
-	
+
 	public Hashtable Lieux;
-		
-	
-	int sélectionnée;
+
+	int selectionne;
 	ArrayList<Photo> images;
 
-	public Modèle() {
+	public Modele() {
 		this.Nom_Images = new Hashtable<String, String>();
 		this.Dates = new Hashtable<String, SimpleDateFormat>();
 		this.Tailles = new Hashtable<String, Integer>();
 		//this.Couleurs = new Hashtable<>;
 		this.Lieux = new Hashtable<String, String>();
+		this.images = new ArrayList<Photo>();
 	}
 
 	public void selectionner(int index) {
-		this.sélectionnée=index;
-		this.notifyObservers(this.images.get(this.sélectionnée));
+		this.selectionne=index;
+		this.notifyObservers(this.images.get(this.selectionne));
 		this.setChanged();
+	}
+
+	public void triAlpha(){
+		ArrayList<Photo> sorted = new ArrayList<Photo>();
+		ArrayList<Photo> sortinprogress = this.images;
+		Photo maxi = sortinprogress.get(0);
+		while(sortinprogress.size()!=0) {
+			for(int j=1; j<sortinprogress.size(); j++) {
+				String s1=sortinprogress.get(j).nom;
+				int k =0;
+				while(k<5 && maxi.nom.charAt(k)==s1.charAt(k)) {
+					k+=1;
+				}
+				if(maxi.nom.charAt(k)<s1.charAt(k)) {
+					maxi.nom = s1;
+				}
+			}
+			sortinprogress.remove(maxi);
+			sorted.add(maxi);
+			
+		}
 	}
 
 }
